5a8e137ea03fe56a2e1ebb0ca7330bb5a7850a74
lucassantoscrfbezerra <lucassantoscrfbezerra@gmail.com>
2020-01-07 15:25:06 -0300

    ADD: parte de validação em registro

--- app/Http/Controllers/AutentificadorController.php
+++ app/Http/Controllers/AutentificadorController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class AutentificadorController extends Controller
 {
-    //
+    public function registro(Request $request)
+    {
+        //parte de validadação
+        $request->validate([
+            'name' => 'required|string',
+            'password' => 'required|string|confirmed',
+            'email' => 'required|string|email|unique:users'
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+    }
+
+    public function logout(Request $request)
+    {
+    }
 }
