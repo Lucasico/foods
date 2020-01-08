aa389e864a386d3fe64b2c2766ef9be8d28fb1a9
lucassantoscrfbezerra <lucassantoscrfbezerra@gmail.com>
2020-01-08 11:16:51 -0300

    MODIFIELD: validação de email já cadastrado

--- app/Http/Controllers/AutentificadorController.php
+++ app/Http/Controllers/AutentificadorController.php
@@ -5,31 +5,41 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AutentificadorController extends Controller
 {
     public function registro(Request $request)
     {
-        //parte de validadação
-        $request->validate([
-            'name' => 'required|string',
-            'password' => 'required|string|confirmed',
-            'email' => 'required|string|email|unique:users'
-        ]);
-        //criando o usuario
-        $user = new User([
-            'name' => $request->name,
-            'password' => bcrypt($request->password),
-            'email' => $request->email
-        ]);
-        //salvando
-        $user->save();
+        $emailCadastrado = DB::table('users')->where('email', $request->email)->value('email');
+        $erroCadastroEmail = "E-mail ja cadastrado";
 
-        return response()->json([
-            'res' => 'Usuario criado com sucesso'
-        ], 201);
+        if ($emailCadastrado != null) {
+            return response()->json(
+                $erroCadastroEmail
+            );
+        } else {
+            //parte de validadação
+            $request->validate([
+                'name' => 'required|string',
+                'password' => 'required|string|confirmed',
+                'email' => 'required|string|email|unique:users'
+            ]);
+
+            //criando o usuario
+
+            $user = new User([
+                'name' => $request->name,
+                'password' => bcrypt($request->password),
+                'email' => $request->email
+            ]);
+            //salvando
+            $user->save();
+            return response()->json([
+                'res' => 'Usuario criado com sucesso'
+            ], 201);
+        }
     }
-
     public function login(Request $request)
     {
         //verificar campos vazios
