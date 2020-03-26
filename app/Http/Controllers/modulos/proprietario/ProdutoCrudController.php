b7183ffed7fc9ce2bde4ab03f73571b150d60bb4
lucassantoscrfbezerra <lucassantoscrfbezerra@gmail.com>
2020-03-26 18:27:09 -0300

    Algoritmo para percorrer pelos relacionamentos users->pessoas->empresa

--- app/Http/Controllers/modulos/proprietario/ProdutoCrudController.php
+++ app/Http/Controllers/modulos/proprietario/ProdutoCrudController.php
@@ -12,7 +12,22 @@ use App\Empresas;
 
 class ProdutoCrudController extends Controller
 {
-    public function storeProdutoEmpresa(){
-    
+    public function storeProdutoEmpresa(Request $request){
+        //user_id do usuario logado no sistema
+        $user_id = $request->user()->id;
+        //pessoa_id do usuario logado no sistema
+        $pessoa_id = User::find($user_id)->pessoa->id;
+        //empresa_id da pessoa logada no sistema
+        $empresa_id = Pessoas::find($pessoa_id)->empresa->id;
+
+            //crtl + k + crtl + c para comentar varias linhas
+            //crtl + k + crtl + u para descomentar varias linhas
+
+        return response()->json([
+            $empresa_id
+        ]);
+
+        
+   
     }
 }
