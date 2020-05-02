f7452977180f89db5a8b732414948017dec3e8c1
unknown <lucassantoscrfbezerra@gmail.com>
2020-05-02 20:45:26 -0300

    TEste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    marcio garganta profunda
                 </div>
 
                 <div class="links">
