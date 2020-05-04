2712f0df72dd55319e02a2c5e47da812dd769066
unknown <lucassantoscrfbezerra@gmail.com>
2020-05-04 10:38:34 -0300

    MODIFICAÇÂO do retorno json nas rotas dashboard

--- app/Http/Controllers/modulos/master/dashboard/DashboardMasterController.php
+++ app/Http/Controllers/modulos/master/dashboard/DashboardMasterController.php
@@ -13,7 +13,7 @@ class DashboardMasterController extends Controller
     public function quantidadeEmpresas(){
         try{
             $countEmpresa = count(Empresas::all());
-            return response()->json(['Quantidade de empresas: ' => $countEmpresa],200);
+            return response()->json($countEmpresa,200);
         }catch(\Exception $e){
             if(config('app.debug')){
                 return response()->json(ApiErros::erroMensageCadastroEmpresa($e->getMessage(),1022));
@@ -26,7 +26,7 @@ class DashboardMasterController extends Controller
     public function quantidadeClientes(){
         try{
             $quantCliente = User::where('permissoes_id',4)->count();
-            return response()->json(['Quantidade de clientes:' => $quantCliente],200);
+            return response()->json($quantCliente,200);
         }catch(\Exception $e){
             if(config('app.debug')){
                 return response()->json(ApiErros::erroMensageCadastroEmpresa($e->getMessage(),1023));
@@ -39,7 +39,7 @@ class DashboardMasterController extends Controller
     public function quantidadeProdutos(){
         try{
             $quantProdutos = count(Produtos::all());
-            return response()->json(['Quantidade de produtos' => $quantProdutos],200);
+            return response()->json($quantProdutos,200);
         }catch(\Exception $e){
             if(config('app.debug')){
                 return response()->json(ApiErros::erroMensageCadastroEmpresa($e->getMessage(),1024));
