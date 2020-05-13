0b6787d125a86f8e681e5ce4f79591ba6a512704
unknown <lucassantoscrfbezerra@gmail.com>
2020-05-13 11:15:04 -0300

    ADD: de metodo de filtragem de pessoa fisica

--- app/Http/Controllers/modulos/master/proprietarios/BuscarProprietarioController.php
+++ app/Http/Controllers/modulos/master/proprietarios/BuscarProprietarioController.php
@@ -12,33 +12,64 @@ use Illuminate\Support\Facades\DB;
 class BuscarProprietarioController extends Controller
 {
     public function filtrarPessoaEmpresa(){
-        $query = DB::table('pessoas')
-        //nome da empresa
-        ->when(Request()->input('nome_empresa'), function($query){
+      if(
+        is_null(Request()->input('razao_social')) &&
+        is_null(Request()->input('cpf')) &&
+        is_null(Request()->input('nome')) &&
+        is_null(Request()->input('usuario')) &&
+        is_null(Request()->input('cidade')) &&
+        is_null(Request()->input('rua')) &&
+        is_null(Request()->input('bairro'))
+      ){
+         return response()->json(["Nenhum campo de busca preenchido, por favor tente novamente"],200);
+      }
+        $query = DB::table('pessoas')->join('empresas','pessoas.empresas_id','=','empresas.id')
+                                     ->join('users','pessoas.id','=','users.pessoas_id')
+                                     ->select('pessoas_id','pessoas.nome', 'pessoas.sexo','pessoas.telefone',
+                                              'pessoas.cpf','pessoas.cidade','pessoas.rua','pessoas.cep',
+                                              'pessoas.bairro','users.name','users.email','empresas.razao_social'
+                                             )
 
-            $query->select('pessoas_id','users.name', 'empresas.razao_social')
-                  ->from('pessoas')->join('empresas','pessoas.empresas_id','=','empresas.id')
-                  ->join('users','pessoas.id','=','users.pessoas_id')
-                  ->where('empresas.razao_social',Request()->input('nome_empresa'));          
-          
-                  
-                  
-                 
-         })
+        //nome da empresa
+        ->when(Request()->input('razao_social'), function($query){
+            $query->where('empresas.razao_social',Request()->input('razao_social'));
+        })
 
          //cpf
-         ->when(Request()->input('cpf'), function($query){
-            $query->where('cpf',Request()->input('cpf'))
-                  ->select('pessoas.nome','pessoas.sexo','pessoas.telefone','pessoas.cpf','pessoas.cidade','pessoas.rua','pessoas.cep','pessoas.bairro');
-         })
+        ->when(Request()->input('cpf'), function($query){
+            $query->where('pessoas.cpf',Request()->input('cpf'));
+        })
 
-          ->paginate(10);
+        //nome pessoal
+        ->when(Request()->input('nome'), function($query){
+          $query->where('pessoas.nome',Request()->input('nome'));
+        })
 
-          if($query->isEmpty()){
-            return response()->json("Nenhuma Empresa encontrada!",200);
-          }
-        
+        //nome de usuaria
+        ->when(Request()->input('usuario'), function($query){
+          $query->where('users.name',Request()->input('usuario'));
+        })
+
+        //cidade
+        ->when(Request()->input('cidade'), function($query){
+          $query->where('pessoas.cidade',Request()->input('cidade'));
+        })
+
+        //rua
+        ->when(Request()->input('rua'), function($query){
+          $query->where('pessoas.rua',Request()->input('rua'));
+        })
+
+        //bairro
+        ->when(Request()->input('bairro'), function($query){
+          $query->where('pessoas.bairro',Request()->input('bairro'));
+        })
+
+        ->paginate(10);
+
+        if($query->isEmpty()){
+          return response()->json("Nenhuma pessoa encontrada!",200);
+        }
           return response()->json($query,200); 
     }
-   
 }
